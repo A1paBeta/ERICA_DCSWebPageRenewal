fafcc1ade8058e28fd772be83f0b9018da5029d3
A1paBeta <alp69799@hanyang.ac.kr>
2019-11-19 19:26:42 +0900

    upload index.php(slider + css standard)

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>한양대학교 ERICA 소프트웨어학부</title>
+    <!-- 상단 제목줄 사이트 소개 아이콘 추가 -->
+    <link href="./assets/HYU_symbol_basic_png.ico" rel="shortcut icon" type="image/x-icon">
+    <!-- CSS3 불러오기(link) -->
+    <link rel="stylesheet" href="./styles/common_style.css">
+    <link rel="stylesheet" href="./styles/index_style.css">
+  </head>
+  <body>
+    <article class="img-slide">
+      <div class="slide-img">
+
+      </div>
+    </article>
+  </body>
+</html>
