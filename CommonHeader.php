64f36188febd5dfed2eb62ba2609365e53026482
A1paBeta <alp69799@hanyang.ac.kr>
2019-12-21 16:37:52 +0900

    Add Right information contents div, hyperlink button and onclick href

---
+++ CommonHeader.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>한양대학교 ERICA 소프트웨어학부</title>
+    <!-- 상단 제목줄 사이트 소개 아이콘 추가 -->
+    <link href="./assets/HYU_symbol_basic_png.ico" rel="shortcut icon" type="image/x-icon">
+    <!-- CSS3 불러오기(link) -->
+    <link rel="stylesheet" href="./styles/common_style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css">
+    <!--JQuery 불러오기(CDN) -->
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js" charset="utf-8"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>한양대학교 ERICA 소프트웨어학부</title>
-    <!-- 상단 제목줄 사이트 소개 아이콘 추가 -->
-    <link href="./assets/HYU_symbol_basic_png.ico" rel="shortcut icon" type="image/x-icon">
-    <!-- CSS3 불러오기(link) -->
-    <link rel="stylesheet" href="./styles/common_style.css">
+<?php
+  include "./CommonHeader.php";
+?>
     <link rel="stylesheet" href="./styles/index_style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css">
-    <!--JQuery 불러오기(CDN) -->
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" charset="utf-8"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js" charset="utf-8"></script>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <script>
       $(document).ready(function(){var imagesCount=7,nowImgNo=2;var totalHTMLStr="";for(var i=1;i<=imagesCount;++i){if(i!=1){totalHTMLStr+="<img id='"+i+"_img' src='./assets/"+i+".png' alt='' style='visibility:hidden;'>"}else{totalHTMLStr+="<img id='"+i+"_img' src='./assets/"+i+".png' alt='' style='visibility:visible;'>"}}$('.slides').html(totalHTMLStr);setInterval(function(){var imgId="#"+nowImgNo+"_img";if(nowImgNo-1<=0){var prevImgId="#"+imagesCount+"_img";$(prevImgId).css("visibility","hidden")}else{var prevImgId="#"+(nowImgNo-1)+"_img";$(prevImgId).css("visibility","hidden")}$(imgId).css("visibility","visible");nowImgNo=nowImgNo+1;if(nowImgNo>imagesCount)nowImgNo=1},2500)});
     </script>
@@ -28,6 +15,20 @@
         </div>
       </div>
 
+      <div class="contents">
+        <div class="notice">
+          <h4>한양대학교 ERICA</h4></br></br>
+          <h4>소프트웨어학부 홈페이지에</h4></br></br>
+          <h4>방문해주셔서 감사합니다.</h4></br></br>
+
+          <button type="button" name="button" onclick="./pages/mainPage.php">홈페이지로 들어가기</button>
+          <button type="button" name="button" onclick="window.open('https://www.hanyang.ac.kr/')">한양대학교 홈페이지</button>
+        </div>
+
+      </div>
+
     </article>
   </body>
-</html>
+  <?php
+    include "./CommonTail.php";
+  ?>
