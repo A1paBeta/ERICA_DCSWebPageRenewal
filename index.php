0c1dad173d04f2b5b9f28b6b6ced95aecf369b21
A1paBeta <alp69799@hanyang.ac.kr>
2019-12-21 15:52:49 +0900

    Add JS Slider, Blur div (Finish make slider)

--- index.php
+++ index.php
@@ -8,19 +8,54 @@
     <!-- CSS3 불러오기(link) -->
     <link rel="stylesheet" href="./styles/common_style.css">
     <link rel="stylesheet" href="./styles/index_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css">
+    <!--JQuery 불러오기(CDN) -->
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js" charset="utf-8"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script>
+      $(document).ready(function(){
+        var imagesCount = 7, nowImgNo = 2;
+        var totalHTMLStr = "";
+        for(var i=1;i<=imagesCount;++i){
+          if(i!=1){
+            totalHTMLStr += "<img id='"+i+"_img' src='./assets/"+i+".png' alt='' style='visibility:hidden;'>";
+          }else {
+            totalHTMLStr += "<img id='"+i+"_img' src='./assets/"+i+".png' alt='' style='visibility:visible;'>";
+          }
+        }
+        $('.slides').html(totalHTMLStr);
+
+        setInterval(function(){
+          var imgId = "#" + nowImgNo + "_img";
+          if(nowImgNo - 1 <= 0){
+            var prevImgId = "#" + imagesCount + "_img";
+            $(prevImgId).css("visibility", "hidden");
+          }else {
+            var prevImgId = "#" + (nowImgNo-1) + "_img";
+            $(prevImgId).css("visibility", "hidden");
+          }
+
+          $(imgId).css("visibility", "visible");
+          nowImgNo = nowImgNo + 1;
+          if(nowImgNo > imagesCount) nowImgNo = 1;
+        },2500);
+
+        //$("#2_img").css("visibility", "hidden");
+      });
+    </script>
   </head>
   <body>
     <article class="img-slide">
-      <div class="slide-blur">
-
-      </div>
       <div class="slide-img">
+        <ul class="slides">
+        </ul>
+        <div class="blur">
+        </div>
+      </div>
 
-      </div>
-      <div class="contents-div">
-        <h3>한양대학교 ERICA 소프트웨어학부에</h3>
-        <h3>방문해주신 여러분을 환영합니다.</h3>
-      </div>
     </article>
   </body>
 </html>
